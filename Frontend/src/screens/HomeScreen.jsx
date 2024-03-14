f58975a22d5ce067fb120b8c1e3389c413e4c964
Luan Huanca <luanhuancam@gmail.com>
2024-03-14 00:16:24 -0400

    creacion carpetas y HomeScreen

---
+++ Frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeScreen = () => {
+    return (
+      <div className="content">
+        <h1>Hola Mundo</h1>
+      </div>
+    );
+  };
+  
+  export default HomeScreen;
