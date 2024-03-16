0088146d009b10bcd70bace231537c6d06e96118
Luan Huanca <luanhuancam@gmail.com>
2024-03-16 16:26:28 -0400

    creacion de archivos para el frontend y diseño

--- Frontend/src/screens/HomeScreen.jsx
+++ Frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Titulo from '../components/Titulo';
+import Especificacion from '../components/Especificacion';
+import "./HomeScreen.css"
 
 const HomeScreen = () => {
     return (
       <div className="content">
-        <h1>Hola Mundo</h1>
+        <Titulo fecha={"Hoy"}/>
+        <div className="tabla">
+            <Especificacion number={0} descripcion={"Tiempo estimado"}/>
+            <Especificacion number={1} descripcion={"Tareas a completar"}/>
+            <Especificacion number={0} descripcion={"Tiempo transcurrido"}/>
+            <Especificacion number={0} descripcion={"Tareas completadas"}/>
+        </div>
       </div>
     );
   };
